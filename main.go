0f2f4663006a54e9b1f49cae3c6ee6f9d60bc27b
Nagasharan <nagasharansrg@gmail.com>
2025-08-08 12:55:27 -0500

    First commit

--- main.go
+++ main.go
@@ -5,14 +5,9 @@ import "fmt"
 func main() {
 
 	fmt.Println("###Hello, Welcome to ToDoList app!!!###")
-	//test("Sharan")
 	fmt.Println("1. Apply for one company")
 	fmt.Println("2. Update the resume and analyze which experience to keep or remove")
 	fmt.Println("3. Learn system design basics for 1 hr")
 	fmt.Println("4. Solve 2 leetcode problems ")
 
-}
-
-func test(string2 string) {
-	fmt.Println("Hello," + string2 + "!!!")
 }
