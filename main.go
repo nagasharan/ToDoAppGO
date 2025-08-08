81e9977db810ca2b041c8c42aa2a3fdad0a0234c
Nagasharan <nagasharansrg@gmail.com>
2025-08-08 12:53:33 -0500

    First commitgit add .

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	fmt.Println("1. Apply for one company")
 	fmt.Println("2. Update the resume and analyze which experience to keep or remove")
 	fmt.Println("3. Learn system design basics for 1 hr")
-	fmt.Println("4. Solve 2 leetcode problems")
+	fmt.Println("4. Solve 2 leetcode problems ")
 
 }
 
