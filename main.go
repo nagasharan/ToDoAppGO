862281d8febc859770b69d26a92f88217202e3dc
Nagasharan <nagasharansrg@gmail.com>
2025-08-08 13:14:40 -0500

    variablrs

--- main.go
+++ main.go
@@ -4,10 +4,25 @@ import "fmt"
 
 func main() {
 
+	var taskOne = "Apply for one company"
+	var taskTwo = "Update the resume and analyze which experience to keep or remove"
+	var taskThree = "Learn system design basics for 1 hr"
+	var taskFour = "Solve 2 leetcode problems"
 	fmt.Println("###Hello, Welcome to ToDoList app!!!###")
-	fmt.Println("1. Apply for one company")
-	fmt.Println("2. Update the resume and analyze which experience to keep or remove")
-	fmt.Println("3. Learn system design basics for 1 hr")
-	fmt.Println("4. Solve 2 leetcode problems ")
 
+	fmt.Println("*List of all Todos*")
+	fmt.Println(taskOne)
+	fmt.Println(taskTwo)
+	fmt.Println(taskThree)
+	fmt.Println(taskFour)
+
+	fmt.Println()
+	fmt.Println("*TUTORIALS*")
+	fmt.Println(taskThree)
+	fmt.Println(taskFour)
+
+	fmt.Println()
+	fmt.Println("*JOB APPLICATION*")
+	fmt.Println(taskOne)
+	fmt.Println(taskTwo)
 }
