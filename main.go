772027d0237feaf25852f35cc33d318b26cb99a0
Nagasharan <nagasharansrg@gmail.com>
2025-08-09 19:21:01 -0500

    loops and lists/slices

--- main.go
+++ main.go
@@ -8,21 +8,14 @@ func main() {
 	var taskTwo = "Update the resume and analyze which experience to keep or remove"
 	var taskThree = "Learn system design basics for 1 hr"
 	var taskFour = "Solve 2 leetcode problems"
+
+	//Slice is a variable size but array is fixed size
+	var listOfItems = []string{taskOne, taskTwo, taskThree, taskFour}
 	fmt.Println("###Hello, Welcome to ToDoList app!!!###")
 
 	fmt.Println("*List of all Todos*")
-	fmt.Println(taskOne)
-	fmt.Println(taskTwo)
-	fmt.Println(taskThree)
-	fmt.Println(taskFour)
-
-	fmt.Println()
-	fmt.Println("*TUTORIALS*")
-	fmt.Println(taskThree)
-	fmt.Println(taskFour)
-
-	fmt.Println()
-	fmt.Println("*JOB APPLICATION*")
-	fmt.Println(taskOne)
-	fmt.Println(taskTwo)
+	//fmt.Println("Tasks: ", listOfItems)
+	for index, item := range listOfItems {
+		fmt.Println(index+1, ". "+item)
+	}
 }
